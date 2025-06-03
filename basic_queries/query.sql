44a8212907242acedc75e485ba765d9e595aa2b0
Jiward H Torres <160532492+JHT-Gitt@users.noreply.github.com>
2025-06-03 13:43:09 -0700

    Update query.sql

--- basic_queries/query.sql
+++ basic_queries/query.sql
@@ -1,1 +1,41 @@
-Q1. Table name is Products
+Q1. Table name is Products <br>
+
+Q2. SELECT ProductID, ProductName, UnitPrice
+FROM Products; <br>
+
+Q3. SELECT ProductID, ProductName, UnitPrice
+FROM Products
+ORDER BY UnitPrice ASC;
+
+Q4. SELECT *
+FROM Products
+WHERE UnitPrice <= 7.50; <br>
+
+  Products under $7.50 or less
+  1. Geitost
+  2. Guarana Fantastica
+  3. Konbu
+  4. Filo Mix
+  5. Tourtiere
+
+Q5. SELECT * FROM Products
+WHERE UnitsInStock >=100
+ORDER BY UnitPrice DESC;
+
+  Products quantity at least 100
+    1. Sirop d'erable        2. Grandma's Boysenberry Spread
+    3. Pate chinois          4. Gustaf's Knackerbord
+    5. Inlagd Sill           6. Boston Crab Meat
+    7. Rod Kaviar            8. Sasquatch Ale
+    9. Rhonbrau klosterbier  10. Geitost
+    
+
+Q6.
+
+Q7.
+
+Q8.
+
+
+
+
